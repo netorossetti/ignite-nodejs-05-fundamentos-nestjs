a0adf62d3915f7f10e75813da49b4db2377eeadf
Neto Rossetti <netinho.rossetti@gmail.com>
2023-12-27 13:01:10 -0300

    feat: Conversa entre camadas (Mappers)

--- src/infra/database/prisma/repositories/prisma-questions-repository.ts
+++ src/infra/database/prisma/repositories/prisma-questions-repository.ts
@@ -2,9 +2,13 @@ import { PaginationParams } from "@/core/repositories/pagination-params";
 import { QuestionsRepository } from "@/domain/forum/application/repositories/questions-repository";
 import { Question } from "@/domain/forum/enterprise/entities/question";
 import { Injectable } from "@nestjs/common";
+import { PrismaService } from "../prisma.service";
+import { PrismaQuestionMapper } from "../mappers/prisma-question-mapper";
 
 @Injectable()
 export class PrismaQuestionsRepository implements QuestionsRepository {
+  constructor(private prisma: PrismaService) {}
+
   create(question: Question): Promise<void> {
     throw new Error("Method not implemented.");
   }
@@ -14,8 +18,10 @@ export class PrismaQuestionsRepository implements QuestionsRepository {
   save(question: Question): Promise<void> {
     throw new Error("Method not implemented.");
   }
-  findById(id: string): Promise<Question | null> {
-    throw new Error("Method not implemented.");
+  async findById(id: string): Promise<Question | null> {
+    const question = await this.prisma.question.findUnique({ where: { id } });
+    if (!question) return null;
+    return PrismaQuestionMapper.toDomain(question);
   }
   findQuestionBySlug(slug: string): Promise<Question | null> {
     throw new Error("Method not implemented.");
