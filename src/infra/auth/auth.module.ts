da5c0df0f918458310a7690dc4e5992065610ed0
Flório Neto <floreio.neto@zeusrio.com.br>
2024-01-09 10:58:27 -0300

    feat: Rotas privadas por padrão

--- src/infra/auth/auth.module.ts
+++ src/infra/auth/auth.module.ts
@@ -4,6 +4,8 @@ import { JwtModule } from "@nestjs/jwt";
 import { PassportModule } from "@nestjs/passport";
 import { Env } from "@/infra/env";
 import { JwtStrategy } from "./jwt.strategy";
+import { APP_GUARD } from "@nestjs/core";
+import { JwtAuthGuard } from "./jwt-auth.guard";
 
 @Module({
   imports: [
@@ -14,12 +16,6 @@ import { JwtStrategy } from "./jwt.strategy";
       useFactory(config: ConfigService<Env, true>) {
         const privateKey = config.get("JWT_PRIVATE_KEY", { infer: true });
         const publicKey = config.get("JWT_PUBLIC_KEY", { infer: true });
-
-        console.log(
-          "priveteKey: ",
-          Buffer.from(privateKey, "base64").toString()
-        );
-        console.log("publicKey: ", Buffer.from(publicKey, "base64").toString());
         return {
           signOptions: { algorithm: "RS256" },
           privateKey: Buffer.from(privateKey, "base64"),
@@ -28,6 +24,12 @@ import { JwtStrategy } from "./jwt.strategy";
       },
     }),
   ],
-  providers: [JwtStrategy],
+  providers: [
+    JwtStrategy,
+    {
+      provide: APP_GUARD,
+      useClass: JwtAuthGuard,
+    },
+  ],
 })
 export class AuthModule {}
--- src/infra/http/controllers/create-question.controller.ts
+++ src/infra/http/controllers/create-question.controller.ts
@@ -1,12 +1,5 @@
-import {
-  BadRequestException,
-  Body,
-  Controller,
-  Post,
-  UseGuards,
-} from "@nestjs/common";
+import { BadRequestException, Body, Controller, Post } from "@nestjs/common";
 import { CurrentUser } from "@/infra/auth/current-user.decorator";
-import { JwtAuthGuard } from "@/infra/auth/jwt-auth.guard";
 import { UserPayload } from "@/infra/auth/jwt.strategy";
 import { ZodValidationPipe } from "@/infra/http/pipes/zod-validation-pipes";
 import { z } from "zod";
@@ -22,7 +15,6 @@ type CreateQuestionBodySchema = z.infer<typeof createQuestionBodySchema>;
 const zodValidation = new ZodValidationPipe(createQuestionBodySchema);
 
 @Controller("/questions")
-@UseGuards(JwtAuthGuard)
 export class CreateQuestionController {
   constructor(private createQuestion: CreateQuestionUseCase) {}
 
