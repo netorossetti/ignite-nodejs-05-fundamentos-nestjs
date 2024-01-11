50dcde581e81631b7d9a1bb9a298487c52d34347
Florio Neto <florio.neto@zeusrio.com.br>
2024-01-11 13:43:34 -0300

    feat: Controller: Deletar Pergunta

--- src/domain/forum/application/use-cases/delete-question.ts
+++ src/domain/forum/application/use-cases/delete-question.ts
@@ -2,6 +2,7 @@ import { Result, failure, success } from "@/core/result";
 import { QuestionsRepository } from "../repositories/questions-repository";
 import { NotAllowedError } from "@/core/error/errors/not-allowed-error";
 import { ResourceNotFoundError } from "@/core/error/errors/resource-not-found-error";
+import { Injectable } from "@nestjs/common";
 
 interface DeleteQuestionUseCaseRequest {
   authorId: string;
@@ -13,6 +14,7 @@ type DeleteQuestionUseCaseResponse = Result<
   null
 >;
 
+@Injectable()
 export class DeleteQuestionUseCase {
   constructor(private questionsRepository: QuestionsRepository) {}
 
